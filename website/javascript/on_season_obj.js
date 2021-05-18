3f40661f391c90f979c98964d37299824f01886d
DwayneJ730 <dwaynej.730@gmail.com>
2021-05-18 11:01:17 -0400

    on-season ad, form, and obj

---
+++ website/javascript/on_season_obj.js
@@ -0,0 +1,38 @@
+let activities = new Object();
+    //off-season activities
+    activities.onseason = new Object();
+        //slope
+        activities.onseason.slope = new Object();
+
+            activities.onseason.slope.id = 0;
+            activities.onseason.slope.danger = 1;
+            activities.onseason.slope.name = " ";
+            activities.onseason.slope.lvl = 1;
+
+            //transportation
+            activities.onseason.slope.sled = new Object();
+            activities.onseason.slope.sled.snowboard = new Object();
+            activities.onseason.slope.sled.skii = new Object();
+            activities.onseason.slope.sled.snow_tubing = new Object();
+
+            activities.onseason.slope.sled.snowboard = "snowboard";
+            activities.onseason.slope.sled.skii = "skii"
+            activities.onseason.slope.sled.snow_tubing = "snow tubing";
+            
+                activities.onseason.slope.sled.snowboard.ages = "6 and up"
+                activities.onseason.slope.sled.skii.ages = "5 and up"
+                activities.onseason.slope.sled.snow_tubing.ages = "all";
+                activities.onseason.slope.sled.snow_tubing.num_of_ppl = "1";
+                activities.onseason.slope.sled.snowboard.num_of_ppl = "1";
+                activities.onseason.slope.sled.skii.num_of_ppl = "1"
+                   
+
+                
+               
+                
+
+
+
+
+
+
